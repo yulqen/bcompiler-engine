a13f004b2f6ba04d5099fbe9848bc15f3114dad6
Matthew Lemon <lemon@matthewlemon.com>
2019-07-18 16:04:08 +0100

    more cleaning

--- engine/repository/templates.py
+++ engine/repository/templates.py
@@ -1,6 +1,5 @@
 import json
 import os
-from pathlib import Path
 
 from ..config import Config
 from ..use_cases.parsing import extract_from_multiple_xlsx_files
@@ -12,26 +11,20 @@ class FSPopulatedTemplatesRepo(Repo):
     """A repo that is based on a single data file in the .bcompiler-engine directory.
     """
 
-    def __init__(self, directory_path: Path):
-        super().__init__(directory_path)
-
     def list_as_json(self) -> str:
         "Try to open the data file containing populated data as json."
         try:
             with open(
                     os.path.join(Config.BCOMPILER_LIBRARY_DATA_DIR,
-                                 "extracted_data.dat")) as f:
-                return f.read()
+                                 "extracted_data.dat")) as data_file:
+                return data_file.read()
         except FileNotFoundError:
-            raise
+            raise FileNotFoundError("Cannot find {}".format(data_file))
 
 
 class InMemoryPopulatedTemplatesRepository(Repo):
     """A repo that does no data file reading or writing - just parsing from excel files.
     """
-
-    def __init__(self, directory_path: Path):
-        super().__init__(directory_path)
 
     def list_as_json(self) -> str:
         "Return data from a directory of populated templates as json."
