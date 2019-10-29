ceac55a8362e51fefc04b319927027f18d25faa9
Matthew Lemon <lemon@matthewlemon.com>
2019-10-29 17:06:17 +0000

    check TODO - need to use datamap_reader instead of datamap_check in cli adapter

--- engine/adapters/cli.py
+++ engine/adapters/cli.py
@@ -1,22 +1,55 @@
 # cli adapters
 import logging
+import sys
 from pathlib import Path
 from typing import List
 
 from openpyxl import load_workbook
 
 import engine.use_cases.parsing
-from engine.config import Config
+from engine.config import Config, check_for_blank, check_for_datamap
 from engine.repository.datamap import InMemorySingleDatamapRepository
 from engine.repository.master import MasterOutputRepository
 from engine.repository.templates import (InMemoryPopulatedTemplatesRepository,
                                          MultipleTemplatesWriteRepo)
 from engine.use_cases.output import WriteMasterToTemplates
 from engine.use_cases.parsing import CreateMasterUseCase
-from engine.utils.extraction import data_validation_report
+from engine.utils.extraction import data_validation_report, datamap_check
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(levelname)s - %(message)s", datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s: %(levelname)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+)
 logger = logging.getLogger(__name__)
+
+
+def check_aux_files(config: Config):
+    """Check the presence of the blank template, the datamap and the validity of the datmap"""
+    blank_t = check_for_blank(config)
+    if blank_t[0]:
+        logger.info(f"Blank template named {blank_t[1]} present in input directory.")
+    else:
+        logger.critical(
+            f"No blank template present. Config requires a file called "
+            f"{config.config_parser['DEFAULT']['blank file name']} in input directory."
+        )
+        sys.exit(0)
+    dm_t = check_for_datamap(config)
+    if dm_t[0]:
+        logger.info(f"Datamap file named {dm_t[1]} present in input directory.")
+    else:
+        logger.critical(
+            f"No datamap file present. Config requires a file called "
+            f"{config.config_parser['DEFAULT']['datamap file name']} in input directory."
+        )
+        sys.exit(0)
+    # TODO: instead of datamap_check - we should use datamap_reader instead!
+    if datamap_check("/home/lemon/Documents/datamaps/input/datamap.csv"):
+        logger.info("Datamap file passes tests. Ok to proceed.")
+    else:
+        logger.critical("Datamap tests failed")
+
 
 
 def report_data_validations_in_file(file: Path) -> List[str]:
@@ -39,7 +72,9 @@ def report_data_validations_in_file(file: Path) -> List[str]:
     return output
 
 
-def write_master_to_templates(blank_template: Path, datamap: Path, master: Path) -> None:
+def write_master_to_templates(
+    blank_template: Path, datamap: Path, master: Path
+) -> None:
     output_repo = MultipleTemplatesWriteRepo(blank_template)
     uc = WriteMasterToTemplates(output_repo, datamap, master, blank_template)
     uc.execute()
@@ -53,16 +88,15 @@ def import_and_create_master(echo_funcs):
 
     Create master spreadsheet immediately.
     """
-    #patch ECHO_FUNC for datamap creation - hack!
+    # patch ECHO_FUNC for datamap creation - hack!
     setattr(engine.use_cases.parsing, "ECHO_FUNC_GREEN", echo_funcs["click_echo_green"])
     setattr(engine.use_cases.parsing, "ECHO_FUNC_RED", echo_funcs["click_echo_red"])
-    setattr(engine.use_cases.parsing, "ECHO_FUNC_YELLOW", echo_funcs["click_echo_yellow"])
+    setattr(
+        engine.use_cases.parsing, "ECHO_FUNC_YELLOW", echo_funcs["click_echo_yellow"]
+    )
     setattr(engine.use_cases.parsing, "ECHO_FUNC_WHITE", echo_funcs["click_echo_white"])
 
-
-    tmpl_repo = InMemoryPopulatedTemplatesRepository(
-        Config.PLATFORM_DOCS_DIR / "input"
-    )
+    tmpl_repo = InMemoryPopulatedTemplatesRepository(Config.PLATFORM_DOCS_DIR / "input")
     master_fn = Config.config_parser["DEFAULT"]["master file name"]
     dm_fn = Config.config_parser["DEFAULT"]["datamap file name"]
     dm = Path(tmpl_repo.directory_path) / dm_fn
@@ -73,4 +107,8 @@ def import_and_create_master(echo_funcs):
         uc.execute(master_fn)
     except FileNotFoundError as e:
         raise FileNotFoundError(e)
-    logger.info("{} successfully created in {}\n".format(master_fn, Path(Config.PLATFORM_DOCS_DIR / "output")))
+    logger.info(
+        "{} successfully created in {}\n".format(
+            master_fn, Path(Config.PLATFORM_DOCS_DIR / "output")
+        )
+    )
