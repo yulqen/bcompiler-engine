00e2860492645215ec4fd8ec5d10a70dd1f55ef9
MR Lemon <matt@matthewlemon>
2020-05-16 16:50:37 +0100

    tests pass on 3.6?

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,9 @@
 import nox
 
 
-@nox.session(python=["3.7", "3.8"])
+# Thanks https://cjolowicz.github.io/posts/hypermodern-python-02-testing/
+
+@nox.session(python=["3.6", "3.7", "3.8"])
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest")
