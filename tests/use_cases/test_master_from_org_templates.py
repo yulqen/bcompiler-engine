8c9c31c422a610c80fbc3f2f47857a8724e0cddf
Matthew Lemon <lemon@matthewlemon.com>
2020-06-22 16:49:11 +0100

    quick clean

--- tests/use_cases/test_master_from_org_templates.py
+++ tests/use_cases/test_master_from_org_templates.py
@@ -13,7 +13,6 @@ from engine.use_cases.parsing import CreateMasterUseCase
 from engine.utils.extraction import datamap_reader, template_reader
 
 
-@pytest.mark.slow
 def test_datamap_reader(mock_config, org_test_files_dir):
     mock_config.initialise()
     for fl in os.listdir(org_test_files_dir):
@@ -61,8 +60,3 @@ def test_create_master_spreadsheet(mock_config, org_test_files_dir):
     ws = wb.active
     assert ws["A1"].value == "file name"
     assert "dft1_tmp" in ws["B1"].value
-    # reintroduce this test once can be locked
-
-
-#   assert ws["B2"].value == "2019-10-20T00:00:00"
-#   assert ws["B3"].value == "This is a string"
