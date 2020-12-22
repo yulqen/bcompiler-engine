6041cf5188d417fc953d61bc6f27c7e49118dbaa
Matthew Lemon <lemon@matthewlemon.com>
2020-12-22 08:28:28 +0000

    change noxfile?

--- noxfile.py
+++ noxfile.py
@@ -6,4 +6,5 @@ import nox
 @nox.session(python=["3.7", "3.8", "3.9"])
 def tests(session):
     session.run("poetry", "install", external=True)
+    session.run("poetry", "update", external=True)
     session.run("pytest")
