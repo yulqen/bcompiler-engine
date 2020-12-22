a4ea90269aef2e49f29d1b71bbc9c174155a3fb0
Matthew Lemon <lemon@matthewlemon.com>
2020-12-22 10:54:41 +0000

    removed 3.9 from noxfile

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import nox
 
 # Thanks https://cjolowicz.github.io/posts/hypermodern-python-02-testing/
 
-@nox.session(python=["3.7", "3.8", "3.9"])
+@nox.session(python=["3.7", "3.8"])
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("poetry", "update", external=True)
